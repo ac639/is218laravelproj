3607a22c33fc352fc38404ee0c0805c3810e85fd
andre <ac639@njit.edu>
2019-07-02 22:57:50 -0400

    Changed About to About Me and removed search bar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -13,14 +13,10 @@
                         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/discover-online-courses">About</a>
+                        <a class="nav-link" href="/discover-online-courses">About Me</a>
                     </li>
 
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                    <button class="btn btn btn-success my-2 my-sm-0" type="submit">Find Courses</button>
-                </form>
             </div>
         </div>
     </nav>
